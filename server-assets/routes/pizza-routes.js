47806d2e37887897bbe0160c1037989f6c11de64
Christopher Brown <chbphone55@gmail.com>
2018-08-21 10:36:20 -0600

    allow index of pizza get request

--- server-assets/routes/pizza-routes.js
+++ server-assets/routes/pizza-routes.js
@@ -16,3 +16,12 @@ const pizzas = [
 ];
 
 router.get('/', (_, res) => res.send(pizzas));
+router.get('/:index', (req, res) => {
+  const index = parseInt(req.params.index);
+  if (index === NaN || index >= pizzas.length || index < 0) {
+    return res.status(400).send({
+      error: 'Out of range'
+    });
+  }
+  res.send(pizzas[index]);
+});
